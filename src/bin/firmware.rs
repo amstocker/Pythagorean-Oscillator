0726f7f6188b99604be73af95bed0daf11c4efb3
Andrew Stocker <sleepyams@gmail.com>
2025-03-08 11:40:38 -0500

    reading from SD card

--- src/bin/firmware.rs
+++ src/bin/firmware.rs
@@ -6,6 +6,13 @@ use prism_firmware as _; // global logger + panicking-behavior + memory layout
 
 #[rtic::app(device = stm32h7xx_hal::pac, peripherals = true)]
 mod app {
+    //use embedded_sdmmc::{SdCard, VolumeManager, Mode, VolumeIdx};
+    use stm32h7xx_hal::{
+        prelude::*,
+        pac,
+        gpio::Speed,
+        sdmmc::{SdCard, Sdmmc},
+    };
     use daisy::audio::Interface;
 
     #[shared]
@@ -18,7 +25,6 @@ mod app {
 
     #[init]
     fn init(cx: init::Context) -> (Shared, Local) {
-        // Get device peripherals.
         let mut cp = cx.core;
         let dp = cx.device;
 
@@ -28,18 +34,86 @@ mod app {
         // This crate already handles that for audio processing.
         cp.SCB.enable_dcache(&mut cp.CPUID);
 
-        // Initialize the board abstraction.
         let board = daisy::Board::take().unwrap();
 
-        // Configure board's peripherals.
         let ccdr = daisy::board_freeze_clocks!(board, dp);
         let pins = daisy::board_split_gpios!(board, ccdr, dp);
+        let mut led_user = daisy::board_split_leds!(pins).USER;
+        let one_second = ccdr.clocks.sys_ck().to_Hz();
+        
         let audio_interface = daisy::board_split_audio!(ccdr, pins);
-
-        // Start audio processing and put its abstraction into a global.
         let audio_interface = audio_interface.spawn().unwrap();
 
-        defmt::debug!("Finished init.");
+
+        // SD Card
+        let (clk, cmd, d0, d1, d2, d3) = (
+            pins.GPIO.PIN_6,
+            pins.GPIO.PIN_5,
+            pins.GPIO.PIN_4,
+            pins.GPIO.PIN_3,
+            pins.GPIO.PIN_2,
+            pins.GPIO.PIN_1,
+        );
+
+        let clk = clk
+            .into_alternate::<12>()
+            .internal_pull_up(false)
+            .speed(Speed::VeryHigh);
+        let clk = clk
+            .into_alternate()
+            .internal_pull_up(false)
+            .speed(Speed::VeryHigh);
+        let cmd = cmd
+            .into_alternate()
+            .internal_pull_up(true)
+            .speed(Speed::VeryHigh);
+        let d0 = d0
+            .into_alternate()
+            .internal_pull_up(true)
+            .speed(Speed::VeryHigh);
+        let d1 = d1
+            .into_alternate()
+            .internal_pull_up(true)
+            .speed(Speed::VeryHigh);
+        let d2 = d2
+            .into_alternate()
+            .internal_pull_up(true)
+            .speed(Speed::VeryHigh);
+        let d3 = d3
+            .into_alternate()
+            .internal_pull_up(true)
+            .speed(Speed::VeryHigh);
+
+        let mut sdmmc: Sdmmc<_, SdCard> = dp.SDMMC1.sdmmc(
+            (clk, cmd, d0, d1, d2, d3),
+            ccdr.peripheral.SDMMC1,
+            &ccdr.clocks,
+        );
+
+        let bus_frequency = 2.MHz();
+
+        while sdmmc.init(bus_frequency).is_err() {
+            led_user.toggle();
+            cortex_m::asm::delay(one_second / 8);
+        }
+
+        let mut buffer = [0x34; 512];
+        defmt::debug!("Writting to the card");
+        sdmmc.write_block(0, &buffer).unwrap();
+        defmt::debug!("Reading from the card");
+        sdmmc.read_block(0, &mut buffer).unwrap();
+        for byte in buffer.iter() {
+            assert_eq!(*byte, 0x34);
+        }
+        defmt::debug!("All went as expected");
+
+        // Keep blinking to block main and shows signs of life and to show that
+        // the test above passed.
+        let one_second = ccdr.clocks.sys_ck().to_Hz();
+        loop {
+            led_user.toggle();
+            cortex_m::asm::delay(one_second);
+        }
 
         (
             Shared {},
@@ -49,9 +123,6 @@ mod app {
         )
     }
 
-    // Audio is tranfered from the input and to the input periodically thorugh DMA.
-    // Every time Daisy is done transferring data, it will ask for more by triggering
-    // the DMA 1 Stream 1 interrupt.
     #[task(binds = DMA1_STR1, local = [audio_interface])]
     fn dsp(cx: dsp::Context) {
         let audio_interface = cx.local.audio_interface;
